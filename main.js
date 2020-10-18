13f76e3adaa61d892be9d0eb3c69598671095100
권세진 <0307kwon@gmail.com>
2020-10-18 22:02:40 +0900

    쿠키 암호화

--- main.js
+++ main.js
@@ -2,17 +2,17 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const app = express();
 
-app.use(cookieParser());
+app.use(cookieParser("bfdnklbffsda!@!$@#%"));
 app.get("/",(req,res)=>{
     let count;
-    if(req.cookies.count === undefined){
+    if(req.signedCookies.count === undefined){
         count = 0;
     }else{
-        count = Number(req.cookies.count);
+        count = Number(req.signedCookies.count);
     }
 
     const count_plus = count+1;
-    res.cookie("count", count_plus);
+    res.cookie("count", count_plus,{signed:true});
     res.send(`count : ${count}`);
 })
 
